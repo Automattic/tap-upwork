ae4ffca33350f7985fa51b33ec8f1d96e32e57e4
Joao Amaral <7281460+joaopamaral@users.noreply.github.com>
2025-01-27 19:59:55 -0300

    test

--- tap_upwork/schemas.py
+++ tap_upwork/schemas.py
@@ -90,7 +90,7 @@ TIME_REPORT_PROPERTIES = PropertiesList(
     Property('memo', StringType, description='Memo associated with the time report'),
     Property(
         'totalHoursWorked',
-        StringType,
+        NumberType,
         description='Total hours worked for the time report',
     ),
     Property(
